1ddec2e77bacbf8848e5c606c3e814af7132f543
nitek10 <60189365+nitek10@users.noreply.github.com>
2020-02-04 03:19:18 +0100

    no message

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="styles.css">
+	 <link href="fontawesome/css/all.css" rel="stylesheet">
+<body>
+	<div class="header_index">
+		<table class="header_table">
+			<tr>
+				<td style="width: 50%; text-align: right; padding-right: 15%;"><i class="fas fa-map-marker-alt"></i> Calle Labastida, 13, 28034 Madrid</td>
+				<td style="float: left; display: contents;"><span><i class="fas fa-phone-alt"></i> 91 729 28 62 </span><span><i class="fas fa-envelope"></i> info@cesfuencarral.com</span><span><i class="fas fa-user-alt"></i> Login / Register</span></td>
+			</tr>
+		</table>
+	</div>
+</body>
+</html>
