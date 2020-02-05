1a5319a65e48c986bd79dfe9f893427b51c2c2ae
nitek10 <60189365+nitek10@users.noreply.github.com>
2020-02-05 02:49:18 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -4,7 +4,36 @@
 	<title></title>
 	<link rel="stylesheet" href="styles.css">
 	 <link href="fontawesome/css/all.css" rel="stylesheet">
+
+	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+	 <script>
+function myFunction() {
+  document.getElementById("menu_oculto").style.display = "block";
+}
+
+$(function() {
+
+  // elementos de la lista
+  var menues = $(".menu_classic span a");
+
+  // manejador de click sobre todos los elementos
+  menues.click(function() {
+     // eliminamos active de todos los elementos
+     menues.removeClass("active");
+     // activamos el elemento clicado.
+     $(this).addClass("active");
+  });
+
+});
+
+</script>
+ </head>
 <body>
+<div class="menu_oculto">
+
+</div>
+
 	<div class="header_index">
 		<table class="header_table">
 			<tr>
@@ -12,6 +41,52 @@
 				<td style="float: left; display: contents;"><span><i class="fas fa-phone-alt"></i> 91 729 28 62 </span><span><i class="fas fa-envelope"></i> info@cesfuencarral.com</span><span><i class="fas fa-user-alt"></i> Login / Register</span></td>
 			</tr>
 		</table>
+	</div>
+
+	<div class="header__nav" style=" display:none;">
+			<table><tr style="display: flex;">
+				<td><a href="https://www.brookhousetraining.com/courses/">Courses</a><i class="toggle-sub-menu"></i>
+<!--<ul class=>
+	<li><a href=>Gas</a></li>
+	<li><a href=>Pl</a></li>
+	<li><a href=>AC</a></li>
+	<li >Ap</a></li>
+</ul>-->
+</td>
+<td><a href="">4 Steps</a></td>
+<td><a href="">Why Us</a></td>
+<td><a href="">Open Days</a></td>
+<td><a href="">Calendar</a></td>
+<td><a href="">Contact</a></td>
+<td>
+<img src="carrera.png">
+</td>
+</tr></table>
+</div>
+
+	<div class="menu_classic">
+		<div>
+			<!-- Imagen y texto-->
+		</div>
+		<div>
+			<span class=""><a href="#" class="">Inicio</a></span>
+		</div>
+		<div>
+			<span><a href="#" class="">Ciclos</a></span>
+				<!--mter un sub menu con grado medio y grado superior-->
+		</div>
+		<div>
+			<span><a href="#" class="">Becas</a></span>
+		</div>
+		<div>
+			<span><a href="#" class="">Conocenos</a></span>
+		</div>
+		<div>
+			<span><a href="#" class="">Campo Virtual</a></span>
+		</div>
+		<div>
+			<span><a href="#" class="">Contacto</a></span>
+		</div>
 	</div>
 </body>
 </html>
